c3eb7f50a8df693c7da475ee4f5751e861e241f2
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2015-11-02 22:42:51 +0900

    scaffolding

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var karma = require('karma').server;
+var jscs = require('gulp-jscs');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var fs = require("fs");
+
+
+gulp.task('jscs', function () {
+	return gulp.src(__dirname+'/src/*.js')
+		.pipe(jscs());
+});
+
+gulp.task('test', function(done) {
+    return karma.start({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true
+	}, function(){
+		fs.readFile(__dirname + '/build/reports/coverage/text-summary.txt', "utf-8", function(err, _data) {
+	    	console.log(_data);
+	    });
+	});
+});
+
+gulp.task('merge', function(){
+	return gulp.src(__dirname+'/src/*.js')
+		.pipe(concat('perf-util.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('perf-util.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', function() {
+	gulp.src(__dirname + '/dist/*.js')
+	// Perform minification tasks, etc here
+	.pipe(gulp.dest('./'));
+});
+
+
+gulp.task('default', ['jscs','test','merge','copy']);
---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome'],
+    frameworks: ['jasmine'],
+    reporters: ['progress', 'coverage'],
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/*.js': ['coverage']
+    },
+    coverageReporter: {
+      // specify a common output directory
+      dir: 'build/reports/coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    },
+    files: [
+    'test/wrap.js',
+    'src/perf-util.js',
+    'test/perf/*.test.js'
+    ]
+  });
+};
